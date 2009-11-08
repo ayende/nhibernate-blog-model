5992311d0bc48b612964b86c487dfa30b7f3627b
Ayende Rahien <ayende@ayende.com>
2009-11-08 19:50:22 +0200

    Adding code that will duplicate the non unique object exception

--- Blog.Console/Program.cs
+++ Blog.Console/Program.cs
@@ -23,7 +23,54 @@ namespace Blog.Console
                     .BuildSessionFactory();
                 //new SchemaExport(configuration).Create(true, true);
                 
+                Post post;
+
+                using (var session = sessionFactory.OpenSession())
+                using (var tx = session.BeginTransaction())
+                {
+                    var user = new User
+                    {
+                        CreatedAt = DateTime.Now,
+                        Username = "ayende"
+                    };
+                    session.Save(user);
+                    post = new Post
+                    {
+                        PostedAt = DateTime.Now,
+                        User = user
+                    };
+                    session.Save(post);
+                    tx.Commit();
+                }
+
+                var postId = post.Id;
+
                
+                using (var session = sessionFactory.OpenSession())
+                using (var tx = session.BeginTransaction())
+                {
+                    // get the entity
+                    post = session.Get<Post>(postId); 
+                    // force the association to be eagerly loaded
+                    System.Console.WriteLine(post.User.Username); 
+                    tx.Commit();
+                }
+
+                using (var session = sessionFactory.OpenSession())
+                using (var tx = session.BeginTransaction())
+                {
+                    // get the user for the entity
+                    var anotherUser = session.Get<User>(post.User.Id);
+
+                    // will return false
+                    ReferenceEquals(anotherUser, post.User);
+
+                    // opps, user instance with the same id but with different
+                    // reference was detected
+                    session.SaveOrUpdate(post);
+
+                    tx.Commit();
+                }
 
                
             }
